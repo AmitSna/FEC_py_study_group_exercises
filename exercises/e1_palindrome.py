30ce9514e3d60dd52f3cea16a815c3e312a85073
AmitSna <18194916+AmitSna@users.noreply.github.com>
2020-11-10 17:44:54 -0300

    Refactor incluye frases

--- exercises/e1_palindrome.py
+++ exercises/e1_palindrome.py
@@ -1,4 +1,4 @@
-palindromo = lambda sentencia: print(sentencia.lower() == sentencia.lower()[::-1])
+palindromo = lambda sentencia: print(sentencia.lower().replace(" ", "") == sentencia.lower().replace(" ", "")[::-1])
 
 sentencia = input("Ingrese la frase a validar: ")
 
