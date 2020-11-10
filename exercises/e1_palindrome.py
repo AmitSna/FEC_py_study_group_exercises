716d71dc559bf215fd25b04b4921b2db006d5649
AmitSna <18194916+AmitSna@users.noreply.github.com>
2020-11-10 02:00:42 -0300

    Formato ajustado a requerimientos

--- exercises/e1_palindrome.py
+++ exercises/e1_palindrome.py
@@ -1,5 +1,5 @@
-is_palindrome = lambda phrase: print(["No es Palíndromo", "Es Palíndromo"][phrase.lower() == phrase.lower()[::-1]])
+palindromo = lambda sentencia: print(sentencia.lower() == sentencia.lower()[::-1])
 
-phrase = input("Ingrese la frase a validar: ")
+sentencia = input("Ingrese la frase a validar: ")
 
-is_palindrome(phrase)
+palindromo(sentencia)
