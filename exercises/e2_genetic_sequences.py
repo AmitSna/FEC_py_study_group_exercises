6857f2081b4e8d70b9c42bafa21fcb06d7ce27c4
AmitSna <18194916+AmitSna@users.noreply.github.com>
2020-11-13 05:53:41 -0300

    Corregir typos y Sumar un assert adicional

--- exercises/e2_genetic_sequences.py
+++ exercises/e2_genetic_sequences.py
@@ -9,7 +9,7 @@ def contar_secuencias_geneticas(secuencias):
 	#Opcional 1: Mostrar por consola las secuencias detectadas
 	print("Las secuencias encontradas son: {0}".format(", ".join(["AUG"+seq for seq in secuencias.split("AUG") if seq[-3:] in ["UAA", "UAG", "UGA"]])))
 	
-	#Opcional 2: Indicar cuantos codones contiene cada secuencia
+	#Opcional 2: Indicar cuántos codones contiene cada secuencia
 	print("Las cantidades de codones de cada secuencia son: {0}".format(", ".join([str(len("AUG"+seq)//3) for seq in secuencias.split("AUG") if seq[-3:] in ["UAA", "UAG", "UGA"]])))
 	
 	#Opcional 3: Emplear un diccionario para guardar todas las secuencias
@@ -22,10 +22,10 @@ def contar_secuencias_geneticas(secuencias):
 				registro_secuencias["AUG"+seq] = 1
 	print(f"El registro de secuencias es: {registro_secuencias}\n")
 	
-	#Opcional 4: Guardar las secuencias en un documento de texto, excel o como prefieran
+	#Opcional 4: Guardar las secuencias en un documento
 	import pandas as pd
 	
-	data_frame = pd.DataFrame(list(registro_secuencias.items()),columns = ["Secuencias Encontradas", "Cantidad"])
+	data_frame = pd.DataFrame(list(registro_secuencias.items()), columns = ["Secuencias Encontradas", "Cantidad"])
 	
 	data_frame.to_excel("Secuencias.xlsx") #Crea el excel Secuencias.xlsx en el mismo directorio
 
@@ -38,3 +38,6 @@ contar_secuencias_geneticas("AUGUGA")
 contar_secuencias_geneticas("AUGGGGUaUxAXUA-AGGUaG")
 
 contar_secuencias_geneticas("AUGGGGUAUUAUUAUAGGUAGAUGGGGUAUUAUUAUAGGUAGAUGGGGUAUUAUUAUAGGUAG")
+
+contar_secuencias_geneticas("AUGUAAAUGUAGAUGUGAAUGUGAAUGUGAAUGUAG")
+
