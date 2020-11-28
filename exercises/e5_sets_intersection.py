49f7a47749832e67f5b56a702c4a7fa0e2e43015
AmitSna <18194916+AmitSna@users.noreply.github.com>
2020-11-27 22:56:15 -0300

    Add Exercise 5
    First Commit!

---
+++ exercises/e5_sets_intersection.py
@@ -0,0 +1,10 @@
+#sets_intersection = lambda a, b: [num for num in sorted(list(set(a))) if num in sorted(b)]
+
+def sets_intersection(a, b):
+	return [num for num in sorted(list(set(a))) if num in sorted(b)]
+
+print(sets_intersection([1, 2, 3, 4, 5, 6, 7, 8], [10, 25, 52, 80, 1, 46, 6, 33, 14, 27, 19]))
+print(sets_intersection([1, 1, 1], [2, 2, 2]))
+print(sets_intersection([3, 2, 1], [4, 3, 2, 1]))
+print(sets_intersection([1, 2, 3, 4, 5], [0, 2, 4, 6, 8, 10]))
+print(sets_intersection([6, 7, 1, 2, 1, 3, 4, 5], [7, 8, 1, 3, 2, 1, 7, 3, 7, 10]))
