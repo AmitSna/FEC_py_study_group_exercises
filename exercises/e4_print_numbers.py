6bb9ed085a1015c299264c745fd2815c264e39cb
AmitSna <18194916+AmitSna@users.noreply.github.com>
2020-11-29 14:15:00 -0300

    Remove extra join in both versions

--- exercises/e4_print_numbers.py
+++ exercises/e4_print_numbers.py
@@ -1,7 +1,7 @@
-#print_numeros = lambda lista_nums: print("\n".join(("".join([str(num) for num in lista_nums]))))
+#print_numeros = lambda lista_nums: print("\n".join([str(num) for num in lista_nums]))
 
 def print_numeros(lista_nums):
-	print("\n".join(("".join([str(num) for num in lista_nums]))))
+		print("\n".join([str(num) for num in lista_nums]))
 
 print_numeros([1, 2, 3])
 print_numeros([3, 2, 1])
