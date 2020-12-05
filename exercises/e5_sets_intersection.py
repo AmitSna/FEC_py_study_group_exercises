5d927b42fb1c7593946c3ad77d26f969c3e7219d
AmitSna <18194916+AmitSna@users.noreply.github.com>
2020-12-05 18:58:51 -0300

    Remove optionals list() and sorted() methods
    First sorted() method implicitly converts a set() object into a list() one
    Second sorted() is optional considering the first collection as ordered

--- exercises/e5_sets_intersection.py
+++ exercises/e5_sets_intersection.py
@@ -1,7 +1,7 @@
 #sets_intersection = lambda a, b: [num for num in sorted(list(set(a))) if num in sorted(b)]
 
 def sets_intersection(a, b):
-	return [num for num in sorted(list(set(a))) if num in sorted(b)]
+	return [num for num in sorted(set(a)) if num in b]
 
 print(sets_intersection([1, 2, 3, 4, 5, 6, 7, 8], [10, 25, 52, 80, 1, 46, 6, 33, 14, 27, 19]))
 print(sets_intersection([1, 1, 1], [2, 2, 2]))
